7c68c452feac3096337c74a8b3f704407fb328ec
hwahyeon <hwahyeon@knou.ac.kr>
2023-11-21 04:20:00 +0900

    Add the label for info

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ import os
 import datetime
 from PIL import Image
 import time
+import webbrowser
 
-def toggle_left_monitor(event=None):
+URL = "https://github.com/hwahyeon/py-screenshot-spliter"
+
+def open_webpage(event=None):
+    webbrowser.open(URL)
+
+def check_left_monitor(event=None):
     left_monitor.set(not left_monitor.get())
 
-def toggle_right_monitor(event=None):
+def check_right_monitor(event=None):
     right_monitor.set(not right_monitor.get())
 
 def take_screenshots(event=None):
@@ -20,46 +26,55 @@ def take_screenshots(event=None):
         monitors = sct.monitors[1:]  # 첫 번째 항목(전체 화면)은 제외
 
         for monitor_number, monitor in enumerate(monitors, start=1):
-            # 체크박스 설정에 따라 적절한 모니터의 스크린샷을 촬영
             if (monitor_number == 1 and left_monitor.get()) or (monitor_number == 2 and right_monitor.get()):
                 shot = sct.grab(monitor)
                 img = Image.frombytes('RGB', (shot.width, shot.height), shot.rgb)
-
                 filename = f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_Monitor_{monitor_number}.png"
                 img.save(os.path.join('screenshots', filename))
 
     root.deiconify()
-    label.config(text="Screenshots Saved")
+    label.config(text="Screenshots Saved!")
 
-# GUI 설정
+# GUI setting
 root = tk.Tk()
 root.title("Screenshot App")
-root.geometry("200x130")
+root.geometry("200x170")
 
 if not os.path.exists('screenshots'):
     os.makedirs('screenshots')
 
-# 체크박스 상태를 저장할 변수
+# Values for Checkbox
 left_monitor = IntVar()
 right_monitor = IntVar()
 
-# 체크박스 생성
+# Checkboxs
 left_monitor_check = tk.Checkbutton(root, text="Left Monitor (L)", variable=left_monitor)
 left_monitor_check.pack()
 right_monitor_check = tk.Checkbutton(root, text="Right Monitor (R)", variable=right_monitor)
 right_monitor_check.pack()
 
-# 단축키 바인딩
-root.bind('<Alt-l>', toggle_left_monitor)
-root.bind('<Alt-L>', toggle_left_monitor)  # 대문자 L에 대한 바인딩도 추가
-root.bind('<Alt-r>', toggle_right_monitor)
-root.bind('<Alt-R>', toggle_right_monitor)  # 대문자 r에 대한 바인딩도 추가
+# Binding for Shortcuts
+root.bind('<Alt-l>', check_left_monitor)
+root.bind('<Alt-r>', check_right_monitor)
 root.bind('<Alt-z>', take_screenshots)
-root.bind('<Alt-Z>', take_screenshots)  # 대문자 Z에 대한 바인딩도 추가
 
 button = tk.Button(root, text="Take Screenshots (Alt+Z)", command=take_screenshots)
 button.pack(pady=10)
 
+# Frame for 2 labels ("Created by" + "Name")
+label_frame = tk.Frame(root)
+label_frame.pack(side=tk.BOTTOM)
+
+# "Created by"
+created_label = tk.Label(label_frame, text="Created by")
+created_label.pack(side=tk.LEFT)
+
+# "Name"
+name_label = tk.Label(label_frame, text="hwahyeon", fg="blue", cursor="hand2")
+name_label.pack(side=tk.LEFT)
+name_label.bind("<Button-1>", open_webpage)
+
+# Saved msg
 label = tk.Label(root, text="")
 label.pack(pady=10)
 
