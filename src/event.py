c46f560b8fb169450a2ffd1ad905e110d9099b06
Janghyeon <hwahyeon@knou.ac.kr>
2024-06-24 21:00:56 +0200

    Disable unused code

--- src/event.py
+++ src/event.py
@@ -11,8 +11,8 @@ def check_right_monitor(right_monitor):
     right_monitor.set(not right_monitor.get())
 
 
-def update_ui_texts(button1, button2, left_monitor_check, right_monitor_check, current_language):
-    button1.config(text=get_text(current_language, "take"))
-    button2.config(text=get_text(current_language, "open"))
-    left_monitor_check.config(text=get_text(current_language, "left"))
-    right_monitor_check.config(text=get_text(current_language, "right"))
+# def update_ui_texts(button1, button2, left_monitor_check, right_monitor_check, current_language):
+#     button1.config(text=get_text(current_language, "take"))
+#     button2.config(text=get_text(current_language, "open"))
+#     left_monitor_check.config(text=get_text(current_language, "left"))
+#     right_monitor_check.config(text=get_text(current_language, "right"))
