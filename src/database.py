3ccdcac208f53004fe846bb80b21b83b2336aaa7
Janghyeon <hwahyeon.dev@gmail.com>
2025-07-03 16:03:48 +0200

    fix: Change database creation location

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
 
-DB_PATH = os.path.join(os.path.dirname(__file__), '../config/settings.db')
+DB_PATH = os.path.join(os.path.dirname(__file__), 'settings.db')
 
 
 def init_db():
